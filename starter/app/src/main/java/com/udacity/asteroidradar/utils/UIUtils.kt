7bf733ffc4716bfff2d11ddafbb2b2af3d1605f1
Maja Francetic <francetic.maja@gmail.com>
2020-11-27 12:49:23 +0100

    Update the displaySnackbar function to set the background color to white

--- starter/app/src/main/java/com/udacity/asteroidradar/utils/UIUtils.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/utils/UIUtils.kt
@@ -1,8 +1,13 @@
 package com.udacity.asteroidradar.utils
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
+import com.udacity.asteroidradar.R
 
 fun displaySnackbar(snackbarText: String, view: View) {
-    Snackbar.make(view, snackbarText, Snackbar.LENGTH_SHORT).show()
+    val snackbar = Snackbar.make(view, snackbarText, Snackbar.LENGTH_SHORT)
+    val backgroundColor = ContextCompat.getColor(view.context, R.color.default_text_color)
+    snackbar.view.setBackgroundColor(backgroundColor)
+    snackbar.show()
 }
