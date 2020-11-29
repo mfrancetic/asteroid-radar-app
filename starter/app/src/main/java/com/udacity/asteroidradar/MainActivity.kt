6fb74872d38bc3694b1bb7706cc6ed5a143b8705
Maja Francetic <francetic.maja@gmail.com>
2020-11-29 14:58:37 +0100

    Setup navController and action bar in the MainActivity; define pop behaviour of the DetailFragment

--- starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -2,11 +2,33 @@ package com.udacity.asteroidradar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.udacity.asteroidradar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupNavControllerAndActionBar()
+    }
+
+    private fun setupNavControllerAndActionBar() {
+        navController = this.findNavController(R.id.nav_host_fragment)
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+                || super.onSupportNavigateUp()
     }
 }
